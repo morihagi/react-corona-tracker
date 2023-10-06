961c81d74837aa2872e5bb9271c3cb26e6c8cbfc
morihagi <mrsk1204@gmail.com>
2023-10-06 00:29:39 -0700

    add results

---
+++ src/components/Selector.js
@@ -0,0 +1,30 @@
+import {useState} from "react";
+import countriesJson from "../countries.json";
+
+const Selector = () => {
+  const [country, setCountry] = useState("");
+
+  const getCountryData = () => {
+    fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
+  return(
+    <div>
+      <select onChange={ (e) => setCountry(e.target.value)}>
+        <option>Select A Country</option>
+        { countriesJson.map((country, index) =>
+          <option key={index} value={country.Slug}>
+            {country.Country}
+          </option>
+        )}
+      </select>
+      <button onClick={getCountryData}>
+        Get Data
+      </button>
+    </div>
+  );
+};
+
+export default Selector;
---
+++ src/pages/TopPage.js
@@ -0,0 +1,15 @@
+import Title from "../components/Title";
+import Selector from "../components/Selector";
+import Results from "../components/Results";
+
+const TopPage = () => {
+  return (
+    <div>
+      <Title />
+      <Selector />
+      <Results />
+    </div>
+  );
+};
+
+export default TopPage;
