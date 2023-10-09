6b99e34a51a50b9585940137af9174fb5b080f50
morihagi <mrsk1204@gmail.com>
2023-10-08 17:09:23 -0700

    第2章完了

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,26 +1,14 @@
-import {useState} from "react";
-import countriesJson from "../countries.json";
-
-const Selector = () => {
-  const [country, setCountry] = useState("");
-
-  const getCountryData = () => {
-    fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
-    .then(res => res.json())
-    .then(data => console.log(data))
-  }
-
+const Selector = (props) => {
   return(
-    <div>
-      <select onChange={ (e) => setCountry(e.target.value)}>
+    <div className="selector-container">
+      <select onChange={ (e) => props.setCountry(e.target.value)}>
         <option>Select A Country</option>
-        { countriesJson.map((country, index) =>
+        { props.countriesJson.map((country, index) =>
           <option key={index} value={country.Slug}>
-            {country.Country}
-          </option>
+          {country.Country}</option>
         )}
       </select>
-      <button onClick={getCountryData}>
+      <button onClick={props.getCountryData}>
         Get Data
       </button>
     </div>
--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -2,12 +2,14 @@ import Title from "../components/Title";
 import Selector from "../components/Selector";
 import Results from "../components/Results";
 
-const TopPage = () => {
+const TopPage = (props) => {
   return (
-    <div>
-      <Title />
-      <Selector />
-      <Results />
+    <div className="top-page-container">
+      <div>
+        <Title />
+        <Selector countriesJson={props.countriesJson} setCountry={props.setCountry} getCountryData={props.getCountryData}/>
+        <Results countryData={props.countryData}/>
+      </div>
     </div>
   );
 };
