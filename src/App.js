fcbafff38affb13998cb7a926e02482fb15d04a6
morihagi <mrsk1204@gmail.com>
2023-10-10 00:44:05 -0700

    第5章完了

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import WorldPage from "./pages/WorldPage";
 import './App.css';
 
 function App() {
-  const [country, setCountry] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [country, setCountry] = useState("japan");
   const [countryData, setCountryData] = useState({
     data: "",
     newConfirmed: "",
@@ -16,30 +17,37 @@ function App() {
   });
   const [allCountriesData, setAllCountriesData] = useState([]);
 
-  const getCountryData = () => {
-    fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
-    .then(res => res.json())
-    .then(data => {
-      setCountryData({
-        date: data[data.length -1].Date,
-        newConfirmed: data[data.length -1].Confirmed - data[data.length -2].Confirmed,
-        totalConfirmed: data[data.length -1].Confirmed,
-        newRecovered: data[data.length -1].Recovered - data[data.length -2].Recovered,
-        totalRecovered: data[data.length -1].Recovered,
-      });
-    })
-  }
+  useEffect(() => {
+    const getCountryData = () => {
+      setLoading(true);
+      fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
+      .then(res => res.json())
+      .then(data => {
+        setCountryData({
+          date: data[data.length -1].Date,
+          newConfirmed: data[data.length -1].Confirmed - data[data.length -2].Confirmed,
+          totalConfirmed: data[data.length -1].Confirmed,
+          newRecovered: data[data.length -1].Recovered - data[data.length -2].Recovered,
+          totalRecovered: data[data.length -1].Recovered,
+        });
+        setLoading(false);
+      })
+      .catch(err => alert("Error! Please reload the page."));
+    }
+    getCountryData();
+  }, [country])
 
   useEffect(() => {
     fetch("https://monotein-books.vercel.app/api/corona-tracker/summary")
     .then(res => res.json())
     .then(data => setAllCountriesData(data.Countries))
+    .catch(err => alert("Error! Please reload the page."));
   }, []);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData} countryData={countryData} />} />
+        <Route path='/' element={<TopPage countriesJson={countriesJson} setCountry={setCountry} countryData={countryData} loading={loading}/>} />
         <Route path='world' element={<WorldPage allCountriesData={allCountriesData} />} />
       </Routes>
     </BrowserRouter>
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,13 +1,20 @@
-const Results = (props) =>{
+import Loading from "../components/Loading";
+
+const Results = ({ countryData, loading }) =>{
+  const { date, newConfirmed, totalConfirmed, newRecovered, totalRecovered } = countryData;
   return(
     <div className="results-container">
-      <div>
-        <p>Date: <span>{props.countryData.date}</span></p>
-        <p>New Case: <span>{props.countryData.newConfirmed.toLocaleString()}</span></p>
-        <p>Total Case: <span>{props.countryData.totalConfirmed.toLocaleString()}</span></p>
-        <p>New Recovered: <span>{props.countryData.newRecovered.toLocaleString()}</span></p>
-        <p>Total Recovered: <span>{props.countryData.totalRecovered.toLocaleString()}</span></p>
-      </div>
+      { loading?
+        <Loading />
+        :
+        <div>
+          <p>Date: <span>{date}</span></p>
+          <p>New Case: <span>{newConfirmed.toLocaleString()}</span></p>
+          <p>Total Case: <span>{totalConfirmed.toLocaleString()}</span></p>
+          <p>New Recovered: <span>{newRecovered.toLocaleString()}</span></p>
+          <p>Total Recovered: <span>{totalRecovered.toLocaleString()}</span></p>
+        </div>
+      }
     </div>
   );
 };
